c36c52c309480221b3d554402488621058ce3cef
Farhod Rozikov <fara.rozikov@gmail.com>
2022-12-20 20:44:26 +0500

    babylonian method 3

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def main(S, d):
     Returns:
         float: result
     '''
-    a = (S - d ** 2 ) / 2 * d
+    a = (S - d ** 2 ) / (2 * d)
     b = a + d
-    x = b - 2 * a ** 2 / 2*b
+    x = b - 2 * a ** 2 / (2*b)
     return x
 
 print(main(5, 4))

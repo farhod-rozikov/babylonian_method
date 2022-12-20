a869251e43c702a8b60e70644cee1b827fb40483
Farhod Rozikov <fara.rozikov@gmail.com>
2022-12-20 20:42:39 +0500

    babylonian method 2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main(S, d):
     '''
     a = (S - d ** 2 ) / 2 * d
     b = a + d
-    x = b - 2 * a ** 2 / b
+    x = b - 2 * a ** 2 / 2*b
     return x
 
-print(main(5, 2))
+print(main(5, 4))
